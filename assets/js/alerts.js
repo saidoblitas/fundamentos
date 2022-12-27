80ac40ca97527331e5ff6d5634581b40f253aa0c
Said Oblitas <junioroblitas50@gmail.com>
2022-12-27 04:58:02 -0500

    commit inicial

---
+++ assets/js/alerts.js
@@ -0,0 +1,13 @@
+
+
+ // alert(' Hola Mundo ');
+
+ 
+ // alert ('sigue practicando ')
+
+ // let nombre = prompt('¿Cual es tu nombre');
+ // console.log (nombre )
+ // console.log( '****' + nombre + '****' ); // ''
+
+ const seleccion = confirm('Está seguro de borrar esto?');
+ console.log(seleccion);
